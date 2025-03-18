eb0657101e8892f9de7d550f5876f7fdeacb5961
sayo <olafplazinski23@gmail.com>
2025-03-18 20:43:28 +0100

    set up the AuthServer project, added automatic setup of hosted mariadb, or local sqlite for development. Added basic identity (WIP), added the base migration for both db projects

---
+++ src/AspNet/Shared/EntityFramework/Context/AuthContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Shared.EntityFramework.Entities;
+
+namespace Shared.EntityFramework.Context;
+
+public class AuthContext : IdentityDbContext<SystemUser>
+{
+    public DbSet<SystemUser> SystemUsers { get; set; }
+
+    public AuthContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
