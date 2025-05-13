f3716de5350367d70769077f99d6fcecf0b1f79b
sayo <olafplazinski23@gmail.com>
2025-05-13 16:05:44 +0200

    Added the server port argument in unity, fixed issues with server manager, added exit event onto the server

--- src/AspNet/Manager/ServerManager/Program.cs
+++ src/AspNet/Manager/ServerManager/Program.cs
@@ -3,6 +3,16 @@ using ServerManager.Servers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Console.WriteLine(builder.Environment.ContentRootPath);
+Console.WriteLine(builder.Environment.WebRootPath);
+
+var serverPath = builder.Configuration["ServerPaths:ServerPathWin"];
+if (string.IsNullOrEmpty(serverPath))
+{
+    throw new EntryPointNotFoundException("Server path not found in configuration.");
+}
+ServerEntry.ServerPath = serverPath;
+
 builder.Services.AddSingleton<ServerController>();
 
 var app = builder.Build();
